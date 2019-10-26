839fc6be70b4b7b0d44113e3b1dcdb2182cc5a2c
Kevin Persson <48264633+kevinpersson@users.noreply.github.com>
2019-10-25 19:42:36 -0700

    SSL connection + option to save original file name
    SSL connection + option to save original file name with basename from os.path
    Update of reference for urlopen in Python 3

--- download_images_list_of_links_from_csv.py
+++ download_images_list_of_links_from_csv.py
@@ -6,9 +6,23 @@ Created on Thu Oct 25 10:45:19 2018
 """
 
 # Code to download from list of links:
-# import pandas as pd
+import pandas as pd
 
-from urllib2 import urlopen
+from urllib.requet import urlopen
+# Option for saving original name, see below
+# from os.path import basename
+
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+# Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+# Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
 
 data=pd.read_csv("Images_Train.csv")
 
@@ -17,13 +31,15 @@ data.head()
 i=0
 for line in data.Link_to_the_image:
     if line != 'nan':               # if in the list there are any empty cells
-    try: 
-        res = urlopen(line)
-        imgData = res.read()
-        output = open("Images/img_" + str(i) + ".png",'wb')
-        output.write(imgData)
-    except:
-        print("Exception")
+        try:
+            res = urlopen(line)
+            imgData = res.read()
+            # Option to save the original file name to later transfer products in a CSV file etc
+            # output = open("Images/" + str(basename(res.url)) + ".png",'wb')
+            output = open("Images/img_" + str(i) + ".png", 'wb')
+            output.write(imgData)
+        except:
+            print("Exception")
     else:
         print ("Image not found".format(line))
     print(i)
